43e87e964ce03de9d7c484d0ab5b8d6cf392aa2a
pymarian2020 <81045885+pymarian2020@users.noreply.github.com>
2021-08-05 21:16:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 import gunicorn
+import psycopg2
 import os
 
 app = Flask(__name__)
@@ -46,4 +47,4 @@ def process():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)

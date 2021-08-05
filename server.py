460b7a464299af29cf64cfac87b3480f038189f4
Marian <mariukus@gmail.com>
2021-08-05 21:01:47 +0200

    DB Uri update

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-import psycopg2
 import gunicorn
+import os
 
 app = Flask(__name__)
+db_uri = os.environ.get('DB_URI', None)
 
 #PASSWORD = "password"
 
 #app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg2://postgres:{PASSWORD}@localhost/quotes"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://efmqoftelefpiw:7b6d3b3c84d48b28e9b0f04ea3afa6f74636f610d4051bc333fedfd8a624fdd5@ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/d853dd7mrkl3o9"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
